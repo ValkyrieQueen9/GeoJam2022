76fd38281204547fa3616d7660269624fdfae242
ValkyrieQueen9 <valkyriannagames@gmail.com>
2022-06-26 01:02:51 +0100

    Finished Shape Rotation
    (Mostly) The shape is currently rotating too fast

--- GeoJam2022/Assets/Scripts/Failed Scripts/ShapeMove3.cs
+++ GeoJam2022/Assets/Scripts/Failed Scripts/ShapeMove3.cs
@@ -12,19 +12,15 @@ public class ShapeMove3 : MonoBehaviour
     private Collider2D col;
 
     private bool isDragging;
-
-    public void OnMouseDown()
+    void Start()
     {
-        isDragging = true;
-    }
-
-    public void OnMouseUp()
-    {
-        isDragging = false;
+        myCam = Camera.main;
+        col = GetComponent<Collider2D>();
     }
 
     private void Update()
     {
+        /*
         Vector3 mousePos = myCam.ScreenToWorldPoint(Input.mousePosition);
 
         if (isDragging)
@@ -32,9 +28,31 @@ public class ShapeMove3 : MonoBehaviour
                 screenPos = myCam.WorldToScreenPoint(transform.position);
                 Vector3 vec3 = Input.mousePosition - screenPos;
                 angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;
+
+            if (col == Physics2D.OverlapPoint(mousePos))
+            {
                 float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
                 transform.eulerAngles = new Vector3(0, 0, angle + angleOffset);
             }
         }
+        */
+
+        Vector3 mousePos = myCam.ScreenToWorldPoint(Input.mousePosition);
+
+        if(Input.GetMouseButton(0))
+        {
+            Debug.Log("Button is pressed down");
+            screenPos = myCam.WorldToScreenPoint(transform.position);
+            Vector3 vec3 = Input.mousePosition - screenPos;
+            angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;
+        }
+
+        if (col == Physics2D.OverlapPoint(mousePos))
+        {
+            Vector3 vec3 = Input.mousePosition - screenPos;
+            float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, 0, angle + angleOffset);
+        }
+            }
     }
 
--- GeoJam2022/Assets/Scripts/ShapeMove.cs
+++ GeoJam2022/Assets/Scripts/ShapeMove.cs
@@ -13,25 +13,28 @@ public class ShapeMove : MonoBehaviour
     {
         myCam = Camera.main;
         col = GetComponent<Collider2D>();
-
     }
 
     void Update()
     {
         Vector3 mousePos = myCam.ScreenToWorldPoint(Input.mousePosition);
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButtonDown(0))
         {
+            if (col == Physics2D.OverlapPoint(mousePos))
+            {
                 screenPos = myCam.WorldToScreenPoint(transform.position);
                 Vector3 vec3 = Input.mousePosition - screenPos;
                 angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;
+            }
         }
-        /*
-        if (col == Physics2D.OverlapPoint(mousePos))
-        {
-            Vector3 vec3 = Input.mousePosition - screenPos;
-            float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(0, 0, angle + angleOffset);
-        }
-        */
+
+        if (Input.GetMouseButton(0))
+            if (col == Physics2D.OverlapPoint(mousePos))
+            {
+                Vector3 vec3 = Input.mousePosition - screenPos;
+                float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
+                transform.eulerAngles = new Vector3(0, 0, angle + angleOffset);
+            }
+        
     }
 }
